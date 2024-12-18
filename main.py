b518a0370eac09ecc21f7303dc098d1e6644c818
Lucas Gabriel <lucas.gabriel3.lg@gmail.com>
2024-12-18 11:24:12 -0300

    initial game structure

---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+
+# Initialize the gaming modal
+pygame.init()
+
+# Setting a caption to the gaming modal
+pygame.display.set_caption("Platformer")
+
+# Game settings
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+# Speed the player will move through the screen
+PLAYER_VEL = 5
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tuple(tile))
+
+    pygame.display.update()
+
+
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Yellow.png")
+
+    run = True
+    while run:
+        # Makes it that the maximum game speed resolution is 60FPS, even in a faster computer
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+
+            # If the user chooses to quit, the loop stops.
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+            draw(window, background, bg_image)
+
+    pygame.quit()
+    quit()
+
+
+if __name__ == "__main__":
+    main(window)
